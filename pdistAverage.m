d8210d1e4455da695080b432a597d8f02457e63a
Bulgarin <Bulgarin@users.noreply.github.com>
2015-03-25 00:50:05 -0400

    Add functionality to pdistAverage.m functon

--- pdistAverage.m
+++ pdistAverage.m
@@ -2,3 +2,17 @@
 %It takes a vector of subject names as input and outputs results in the same order
 
 
+function [pdistAnalysisCell] = pdistAverage(nameVector)
+	
+	pdistAnalysisCell = {}
+	pdistAnalysisCell{1,1} = 'subject'
+	pdistAnalysisCell{2,1} = 'visits2'
+	pdistAnalysisCell{3,1} = 'visits3'
+	pdistAnalysisCell{4,1} = 'combined'
+
+	for s = 1:length(nameVector)
+
+		evalString = strcat('/home/cnslab/realtime/summaryData/siftScatterData', nameVector(s))
+		eval(evalString)
+
+		pdistAnalysisCell{1, x+1} = nameVector(x)
