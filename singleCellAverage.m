c0812271e771ed9247c8a956ac5eb50c3b735fd8
Bulgarin <Bulgarin@users.noreply.github.com>
2015-03-25 00:46:56 -0400

    Add singleCellAverage helper function

---
+++ singleCellAverage.m
@@ -0,0 +1,6 @@
+%This is a function to calculate mean(pdist(coords w/ >=3 visits))/mean(all possible coords) for a single class of object
+%This is a helper function to the pdistAverage function, used to return the cell array of one class of object 
+
+
+function [pdistSingleCell] = singleCellAverage(nameVector)
+
