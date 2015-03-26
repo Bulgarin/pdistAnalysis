b9ef125d3f8841b36f67584843c7a3e0065e9cb1
iotzov    Ivan Iotzov <iotzov@puppet.cis.fordham.edu>
2015-03-25 21:04:44 -0400

    Finished singleCellAverage function

--- singleCellAverage.m
+++ singleCellAverage.m
@@ -2,7 +2,7 @@
 %This is a helper function to the pdistAverage function, used to return the cell array of one class of object 
 
 
-function [pdistSingleCell] = singleCellAverage(nameVector, classNum)
+function [pdistSingleCell] = singleCellAverage(name, classNum)
 
 	pdistSingleCell = {}
 	pdistSingleCell{1,1} = 'subject'
@@ -12,23 +12,22 @@ function [pdistSingleCell] = singleCellAverage(nameVector, classNum)
 	pdistSingleCell{2,1} = 'class'
 	pdistSingleCell{2,2} = classNum
 
-	for s = 1:length(nameVector)
 
-		evalString = strcat('/home/cnslab/realtime/summaryData/siftScatterData', nameVector{s})
+
+		evalString = strcat('load /home/cnslab/realtime/summaryData/siftScatterData', name)
 		eval(evalString)
 
-		pdistSingleCell{1, 2} = nameVector{s}
-		pdistSingleCell{3, 2} = mean(pdist(mCoords{classNum}(find(visits2{classNum} >= 3))')) ...
-		/mean(pdist(mCoords{classNum}))
+		pdistSingleCell{1, 2} = name
 
-		pdistSingleCell{4, 1} = mean(pdist(mCoords{classNum}(find(visits3{classNum} >= 3))')) ...
-		/mean(pdist(mCoords{classNum}))
+		visits2Pdist = 	pdist(mCoords{classNum}(find(visits2{classNum} >= 3))')
+		pdistSingleCell{3, 2} = mean(visits2Pdist)/mean(pdist(mCoords{classNum}))
 
-		comb = mean(mean(pdist(mCoords{classNum}find(visits2{classNum} >= 3)')), ...
-			mean(pdist(mCoords{classNum}find(visits3{classNum} >= 3)')))
+		visits3Pdist = pdist(mCoords{classNum}(find(visits3{classNum} >= 3))')
+		pdistSingleCell{4, 2} = mean(visits3Pdist)/mean(pdist(mCoords{classNum}))
+		
+		comb = [mCoords{classNum}(find(visits3{classNum} >= 3))' ; mCoords{classNum}(find(visits2{classNum} >= 3))']
 
-		pdistSingleCell{5,2} = comb
+		pdistSingleCell{5,2} = mean(pdist(comb))/mean(pdist(mCoords{classNum}))
 
-	end
 
 end
